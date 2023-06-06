61a641aecfcf5b4cd33d997f08b302ffdc1bf531
CHARAN TEJA <charan.teja@improve10x.com>
2023-06-06 16:14:43 +0530

    navigation completed

--- app/src/main/java/com/example/quizapp/home/HomeActivity.java
+++ app/src/main/java/com/example/quizapp/home/HomeActivity.java
@@ -2,15 +2,29 @@ package com.example.quizapp.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.quizapp.R;
+import com.example.quizapp.databinding.ActivityHomeBinding;
+import com.example.quizapp.question.QuestionsActivity;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        handleStartBtn();
+    }
+
+    private void handleStartBtn() {
+        binding.startBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(this, QuestionsActivity.class);
+            startActivity(intent);
+        });
     }
 }
