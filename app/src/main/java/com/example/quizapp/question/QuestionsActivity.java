a49276edd6c1e1135b631934818468a6c5954427
CHARAN TEJA <charan.teja@improve10x.com>
2023-06-06 16:04:01 +0530

    Added view binding in build.gradle

---
+++ app/src/main/java/com/example/quizapp/question/QuestionsActivity.java
@@ -0,0 +1,18 @@
+package com.example.quizapp.question;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quizapp.R;
+
+public class QuestionsActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+    }
+}
