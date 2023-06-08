78e192d812e6af682850fc3761c8546e3a9b7b65
CHARAN TEJA <charan.teja@improve10x.com>
2023-06-08 17:24:24 +0530

    code divided as methods

--- app/src/main/java/com/example/quizapp/question/QuestionsActivity.java
+++ app/src/main/java/com/example/quizapp/question/QuestionsActivity.java
@@ -3,6 +3,7 @@ package com.example.quizapp.question;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.quizapp.BaseActivity;
